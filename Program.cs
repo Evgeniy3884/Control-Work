ce4275ade6c6560fa2589a9968319f1ff9fb1d90
evgeniy kliukvin <evg.klukvin@mail.ru>
2022-08-10 22:11:49 +0700

    добавление цикла

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿string [] arrayA = {"Hello","ok","goodbye","Start","atm","big","water","friend","hi","n","cafe"}; 
 int L = 0; 
 int lengthArray=arrayA.Length; 
+for(int i = 0;i<lengthArray;i++) 
+{
+    if(arrayA[i].Length <= 3 ) 
+     L++;
+}
